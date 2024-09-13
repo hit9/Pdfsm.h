df8c4f8c0fd6051c1bb67724c03e66068635b874
hit9 <hit9@icloud.com>
2024-09-13 12:03:49 +0800

    Switch to ue coding style

--- pdfsm.h
+++ pdfsm.h
@@ -26,225 +26,253 @@
 #include <stdexcept>
 #include <type_traits>
 
-namespace pdfsm {
+namespace pdfsm
+{
 
-//////////////////////
-/// Update Context
-//////////////////////
+	//////////////////////
+	/// Update Context
+	//////////////////////
 
-// Ticking context.
-struct Context {
-  // ticking seq number.
-  unsigned long long seq = 0;  // cppcheck-suppress
-  // delta time since last tick.
-  std::chrono::nanoseconds delta;
-  // user data.
-  std::any data;
+	// Ticking context.
+	struct Context
+	{
+		// ticking seq number.
+		unsigned long long seq = 0; // cppcheck-suppress
+		// delta time since last tick.
+		std::chrono::nanoseconds delta;
+		// user data.
+		std::any data;
 
-  Context() = default;
-  explicit Context(std::any data) : data(data) {}
-};
+		Context() = default;
+		explicit Context(std::any data)
+			: data(data) {}
+	};
 
-//////////////////////
-/// State
-//////////////////////
+	//////////////////////
+	/// State
+	//////////////////////
 
-// EnumClass constrains that the given type T must be an integeral enum.
-template <typename T>
-concept EnumClass = std::is_enum_v<T> && std::is_integral_v<std::underlying_type_t<T>>;
+	// EnumClass constrains that the given type T must be an integeral enum.
+	template <typename T>
+	concept EnumClass = std::is_enum_v<T> && std::is_integral_v<std::underlying_type_t<T>>;
 
-template <EnumClass State>
-class StateMachineHandler;  // Forward declaration of StateMachineHandler.
+	// Forward declaration of StateMachineHandler.
+	template <EnumClass State>
+	class StateMachineHandler;
 
-template <EnumClass State>
-class IStateBehaviorBase {
-  using Handler = StateMachineHandler<State>;
+	template <EnumClass State>
+	class IStateBehaviorBase
+	{
+		using Handler = StateMachineHandler<State>;
 
- private:
-  Handler* handler = nullptr;
-  void bindHandler(Handler* h) { handler = h; }
-  friend Handler;
+	protected:
+		Handler& GetHandler() { return *handler; }
 
- protected:
-  Handler& GetHandler() { return *handler; }
-};
+	private:
+		Handler* handler = nullptr;
 
-// StateBehavior interface.
-template <EnumClass State>
-class IStateBehavior : public IStateBehaviorBase<State> {
- public:
-  IStateBehavior() = default;
-  virtual ~IStateBehavior() = default;
-  virtual State StateValue(void) const = 0;
+		void BindHandler(Handler* h) { handler = h; }
 
-  ///////////////////////////////
-  /// Overridable Hook Methods
-  //////////////////////////////
+		friend Handler;
+	};
 
-  virtual void OnSetup() {}
-  virtual void OnEnter(const Context& ctx) {}
-  virtual void OnTerminate(const Context& ctx) {}
-  virtual void OnPause(const Context& ctx) {}
-  virtual void OnResume(const Context& ctx) {}
-  virtual bool BeforeUpdate(const Context& ctx) { return false; }
-  virtual void Update(const Context& ctx) {}
-};
+	// StateBehavior interface.
+	template <EnumClass State>
+	class IStateBehavior : public IStateBehaviorBase<State>
+	{
+	public:
+		IStateBehavior() = default;
+		virtual ~IStateBehavior() = default;
+		virtual State StateValue(void) const = 0;
 
-// internal helper class.
-template <EnumClass State, State EnumValue>
-struct _s {};
+		///////////////////////////////
+		/// Overridable Hook Methods
+		//////////////////////////////
 
-template <auto EnumValue>
-class StateBehavior : public _s<decltype(EnumValue), EnumValue>,
-                      public IStateBehavior<decltype(EnumValue)> {
- protected:
-  using State = decltype(EnumValue);
+		virtual void OnSetup() {}
+		virtual void OnEnter(const Context& ctx) {}
+		virtual void OnTerminate(const Context& ctx) {}
+		virtual void OnPause(const Context& ctx) {}
+		virtual void OnResume(const Context& ctx) {}
+		virtual bool BeforeUpdate(const Context& ctx) { return false; }
+		virtual void Update(const Context& ctx) {}
+	};
 
- public:
-  State StateValue(void) const final override { return EnumValue; }
-};
+	// internal helper class.
+	template <EnumClass State, State EnumValue>
+	struct _s
+	{
+	};
 
-template <auto EnumValue>
-using B = StateBehavior<EnumValue>;  // alias
+	template <auto EnumValue>
+	class StateBehavior :
+		public _s<decltype(EnumValue), EnumValue>,
+		public IStateBehavior<decltype(EnumValue)>
+	{
+	public:
+		using State = decltype(EnumValue);
 
-template <EnumClass State>
-using StateBehaviorTable = std::initializer_list<std::unique_ptr<IStateBehavior<State>>>;
+		State StateValue(void) const final override { return EnumValue; }
+	};
 
-template <EnumClass State>
-using BTable = StateBehaviorTable<State>;  // alias
+	template <auto EnumValue>
+	using B = StateBehavior<EnumValue>; // alias
 
-//////////////////////
-/// Transition
-//////////////////////
+	template <EnumClass State>
+	using StateBehaviorTable = std::initializer_list<std::unique_ptr<IStateBehavior<State>>>;
 
-template <EnumClass State>
-struct Transition {
-  State from;
-  std::initializer_list<State> targets;
-};
+	template <EnumClass State>
+	using BTable = StateBehaviorTable<State>; // alias
 
-template <EnumClass State>
-using TransitionTable = std::initializer_list<Transition<State>>;
+	//////////////////////
+	/// Transition
+	//////////////////////
 
-//////////////////////
-/// StateMachine
-//////////////////////
+	template <EnumClass State>
+	struct Transition
+	{
+		State						 from;
+		std::initializer_list<State> targets;
+	};
 
-// StateMachine is just plain struct storing active states.
-template <EnumClass State>
-struct StateMachine {
-  // N is the max value of this enum class, aka the size.
-  static const int N = static_cast<int>(State::N);
-  // static-array based stack, stores enum value's integers.
-  // The initial state is state 0.
-  // top=-1 meaning this state machine still not started.
-  int stack[N], top = -1;
-};
+	template <EnumClass State>
+	using TransitionTable = std::initializer_list<Transition<State>>;
 
-/////////////////////////
-/// StateMachineHandler
-/////////////////////////
+	//////////////////////
+	/// StateMachine
+	//////////////////////
 
-template <EnumClass State>
-class StateMachineHandler {
- private:
-  // N is the max value of this enum class, aka the size.
-  static const int N = static_cast<int>(State::N);
-  // (Compressed) transition table, tt[from][to]
-  std::bitset<N> tt[N];
-  // Behavior pointers array.
-  // bt[state enum integer] => raw pointer to the behavior instance.
-  IStateBehavior<State>* bt[N];
-  // Currently processing fsm.
-  StateMachine<State>* m = nullptr;
+	// StateMachine is just plain struct storing active states.
+	template <EnumClass State>
+	struct StateMachine
+	{
+		// N is the max value of this enum class, aka the size.
+		static const int N = static_cast<int>(State::N);
+		// static-array based stack, stores enum value's integers.
+		// The initial state is state 0.
+		// top=-1 meaning this state machine still not started.
+		int stack[N], top = -1;
+	};
 
- protected:
-  // throws a runtime_error if the transition is invalid.
-  inline void check(int from, int to) const {
-    if (!tt[from][to])
-      throw std::runtime_error("pdfsm: invalid jump from " + std::to_string(from) +
-                               " to " + std::to_string(to));
-  }
-  inline int C(State state) const { return static_cast<int>(state); }
+	/////////////////////////
+	/// StateMachineHandler
+	/////////////////////////
 
-  // Setup this state machine by a behaviors table and a transitions table.
-  void setup(const StateBehaviorTable<State>& behaviors,
-             const TransitionTable<State>& transitions) {
-    // Setup behaviors.
-    for (auto& b : behaviors) {
-      auto state = b->StateValue();
-      bt[C(state)] = b.get();
-      b->bindHandler(this);
-      b->OnSetup();
-    }
+	template <EnumClass State>
+	class StateMachineHandler
+	{
+	private:
+		// N is the max value of this enum class, aka the size.
+		static const int N = static_cast<int>(State::N);
+		// (Compressed) transition table, tt[from][to]
+		std::bitset<N> tt[N];
+		// Behavior pointers array.
+		// bt[state enum integer] => raw pointer to the behavior instance.
+		IStateBehavior<State>* bt[N];
+		// Currently processing fsm.
+		StateMachine<State>* m = nullptr;
 
-    // Setup transitions.
-    for (const auto& t : transitions)
-      for (const auto& to : t.targets) {
-        tt[C(t.from)][C(to)] = 1;
-      }
-  }
+	protected:
+		// throws a runtime_error if the transition is invalid.
+		inline void Check(int from, int to) const
+		{
+			if (!tt[from][to])
+				throw std::runtime_error("pdfsm: invalid jump from " + std::to_string(from) + " to " + std::to_string(to));
+		}
+		inline int C(State state) const { return static_cast<int>(state); }
 
- public:
-  StateMachineHandler(const auto& behaviors, const auto& transitions) {
-    setup(behaviors, transitions);
-  }
+		// Setup this state machine by a behaviors table and a transitions table.
+		void Setup(const StateBehaviorTable<State>& behaviors,
+			const TransitionTable<State>&			transitions)
+		{
+			// Setup behaviors.
+			for (auto& b : behaviors)
+			{
+				auto state = b->StateValue();
+				bt[C(state)] = b.get();
+				b->BindHandler(this);
+				b->OnSetup();
+			}
 
-  // Sets current handling fsm.
-  void SetHandlingFsm(StateMachine<State>& fsm, const Context& ctx) {
-    m = &fsm;
-    if (m->top == -1) Jump(ctx, static_cast<State>(0));
-  }
+			// Setup transitions.
+			for (const auto& t : transitions)
+				for (const auto& to : t.targets)
+				{
+					tt[C(t.from)][C(to)] = 1;
+				}
+		}
 
-  // Clears current handling fsm.
-  void ClearHandlingFsm(void) { m = nullptr; }
+	public:
+		StateMachineHandler(const auto& behaviors, const auto& transitions)
+		{
+			Setup(behaviors, transitions);
+		}
 
-  // Returns current active state.
-  State Top(void) const {
-    assert(m != nullptr);
-    assert(m->top >= 0);
-    return static_cast<State>(m->stack[m->top]);
-  }
+		// Sets current handling fsm.
+		void SetHandlingFsm(StateMachine<State>& fsm, const Context& ctx)
+		{
+			m = &fsm;
+			if (m->top == -1)
+				Jump(ctx, static_cast<State>(0));
+		}
 
-  // Propagates ticking to current active state.
-  void Update(const Context& ctx) {
-    assert(m != nullptr);
-    if (bt[m->stack[m->top]]->BeforeUpdate(ctx)) return;
-    bt[m->stack[m->top]]->Update(ctx);
-  }
+		// Clears current handling fsm.
+		void ClearHandlingFsm(void) { m = nullptr; }
 
-  // Jump to a state.
-  void Jump(const Context& ctx, const State& to) {
-    assert(m != nullptr);
-    int x = C(to);
-    if (m->top != -1) {
-      check(m->stack[m->top], x);
-      bt[m->stack[m->top--]]->OnTerminate(ctx);
-    }
-    m->stack[++m->top] = x;
-    bt[x]->OnEnter(ctx);
-  }
+		// Returns current active state.
+		State Top(void) const
+		{
+			assert(m != nullptr);
+			assert(m->top >= 0);
+			return static_cast<State>(m->stack[m->top]);
+		}
 
-  // Pause current active state and push a new one.
-  void Push(const Context& ctx, const State& to) {
-    assert(m != nullptr);
-    int x = C(to);
-    if (m->top != -1) {
-      check(m->stack[m->top], x);
-      bt[m->stack[m->top]]->OnPause(ctx);
-    }
-    m->stack[++m->top] = x;
-    bt[x]->OnEnter(ctx);
-  }
+		// Propagates ticking to current active state.
+		void Update(const Context& ctx)
+		{
+			assert(m != nullptr);
+			if (bt[m->stack[m->top]]->BeforeUpdate(ctx))
+				return;
+			bt[m->stack[m->top]]->Update(ctx);
+		}
 
-  // Pop current active state and resume the previous paused state.
-  void Pop(const Context& ctx) {
-    assert(m != nullptr);
-    assert(m->top >= 0);
-    bt[m->stack[m->top--]]->OnTerminate(ctx);
-    bt[m->stack[m->top]]->OnResume(ctx);
-  }
-};
-}  // namespace pdfsm
+		// Jump to a state.
+		void Jump(const Context& ctx, const State& to)
+		{
+			assert(m != nullptr);
+			int x = C(to);
+			if (m->top != -1)
+			{
+				Check(m->stack[m->top], x);
+				bt[m->stack[m->top--]]->OnTerminate(ctx);
+			}
+			m->stack[++m->top] = x;
+			bt[x]->OnEnter(ctx);
+		}
+
+		// Pause current active state and push a new one.
+		void Push(const Context& ctx, const State& to)
+		{
+			assert(m != nullptr);
+			int x = C(to);
+			if (m->top != -1)
+			{
+				Check(m->stack[m->top], x);
+				bt[m->stack[m->top]]->OnPause(ctx);
+			}
+			m->stack[++m->top] = x;
+			bt[x]->OnEnter(ctx);
+		}
+
+		// Pop current active state and resume the previous paused state.
+		void Pop(const Context& ctx)
+		{
+			assert(m != nullptr);
+			assert(m->top >= 0);
+			bt[m->stack[m->top--]]->OnTerminate(ctx);
+			bt[m->stack[m->top]]->OnResume(ctx);
+		}
+	};
+} // namespace pdfsm
 
 #endif
